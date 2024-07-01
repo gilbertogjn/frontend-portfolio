f700aa17d11021ceb89b0ff639875e53a7c2a3cb
Gilberto Garcia <gilbertogjn@gmail.com>
2024-07-01 10:28:05 -0400

    update on about text

--- src/sections/About/index.tsx
+++ src/sections/About/index.tsx
@@ -29,9 +29,13 @@ const About = () => (
             </AboutCard>
           </CardsContainer>
           <p>
-            Sou um entusiasta de tecnologia apaixonado por criar experiências
-            visuais incríveis na web. Especializado em desenvolvimento Frontend
-            com foco em React.
+            Engenheiro Front-end formado pela EBAC – Escola Britânica de Artes
+            Criativas e Tecnologia, com graduação em Análise e Desenvolvimento
+            de Sistemas pela Universidade Metodista de São Paulo. Profissional
+            com perfil resiliente, organizado e atento aos detalhes. Possuo mais
+            de 7 anos de experiência na área de criação para a internet, com
+            foco no design de interfaces para web utilizando as mais recentes
+            tecnologias do mercado, especialmente React.
           </p>
         </div>
       </AboutInfo>
